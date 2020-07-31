db95c063f3a67d199a1e365cf5979c20a20b3b61
jinyong <jinyong1234>
2020-07-31 12:55:02 +0800

    leetcode

--- src/main/java/leetcode/MinNumber.java
+++ src/main/java/leetcode/MinNumber.java
@@ -28,4 +28,14 @@ public class MinNumber {
         }
         return sb.toString();
     }
+
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            res ^=i;
+            res ^=nums[i];
+        }
+        res ^= nums.length;
+        return res;
+    }
 }
